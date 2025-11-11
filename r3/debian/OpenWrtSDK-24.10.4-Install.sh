d6a8a2f593e6e2ade87c259a71470fc67fe2b326
NiPeGun <nipegun@gmail.com>
2025-11-11 04:05:59 +0100

    Update OpenWrtSDK-24.10.4-Install.sh

--- r3/debian/OpenWrtSDK-24.10.4-Install.sh
+++ r3/debian/OpenWrtSDK-24.10.4-Install.sh
@@ -1,25 +1,36 @@
 #!/bin/bash
 
-sudo apt-get -y update
-sudo apt-get -y install build-essential
-sudo apt-get -y install libncurses5-dev
-sudo apt-get -y install libncursesw5-dev
-sudo apt-get -y install rsync
+# ----------
+# Script de NiPeGun para Instalar el SDK de OpenWrt para Mediatek Filogic
+#
+# Ejecución remota (puede requerir permisos sudo):
+#   curl -sL https://raw.githubusercontent.com/nipegun/bpi-scripts/refs/heads/main/r3/debian/OpenWrtSDK-24.10.4-Install.sh | bash
+# ----------
 
+# Instalar requisitos
+  sudo apt-get -y update
+  sudo apt-get -y install build-essential
+  sudo apt-get -y install libncurses5-dev
+  sudo apt-get -y install libncursesw5-dev
+  sudo apt-get -y install rsync
+
+# Crear la carpeta
+  mkdir -p $HOME/OpenWrtSDK/bpi-r3/24.10.4/
+  cd $HOME/OpenWrtSDK/bpi-r3/24.10.4/
 
 # Descargar el SDK correcto
-wget https://downloads.openwrt.org/releases/24.10.4/targets/mediatek/filogic/openwrt-sdk-24.10.4-mediatek-filogic_gcc-13.3.0_musl.Linux-x86_64.tar.zst
+  wget https://downloads.openwrt.org/releases/24.10.4/targets/mediatek/filogic/openwrt-sdk-24.10.4-mediatek-filogic_gcc-13.3.0_musl.Linux-x86_64.tar.zst
 
 # Descomprimir (necesitas zstd instalado)
-tar --zstd -xf openwrt-sdk-24.10.4-mediatek-filogic_gcc-13.3.0_musl.Linux-x86_64.tar.zst
+  tar --zstd -xf openwrt-sdk-24.10.4-mediatek-filogic_gcc-13.3.0_musl.Linux-x86_64.tar.zst
 
 # O si tar no soporta --zstd directamente:
-zstd -d openwrt-sdk-24.10.4-mediatek-filogic_gcc-13.3.0_musl.Linux-x86_64.tar.zst
-tar xf openwrt-sdk-24.10.4-mediatek-filogic_gcc-13.3.0_musl.Linux-x86_64.tar
+  zstd -d openwrt-sdk-24.10.4-mediatek-filogic_gcc-13.3.0_musl.Linux-x86_64.tar.zst
+  tar xf openwrt-sdk-24.10.4-mediatek-filogic_gcc-13.3.0_musl.Linux-x86_64.tar
 
 # Entrar al directorio
-cd openwrt-sdk-24.10.4-mediatek-filogic_gcc-13.3.0_musl.Linux-x86_64
+  cd openwrt-sdk-24.10.4-mediatek-filogic_gcc-13.3.0_musl.Linux-x86_64
 
 # Actualizar feeds
-./scripts/feeds update -a
+  ./scripts/feeds update -a
 
